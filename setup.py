27820f3301a0bb081851c098a17816d8c0cee257
Arghya B5 <arghyabiswa1947@gmail.com>
2025-12-12 11:13:10 +0530

    Add text_split function and chunking logic

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="medical_chatbot",
+    version="0.1.0",
+    author="Arghya Biswas",
+    author_email="arghyabiswa1947@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
